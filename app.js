3d554fbc59b0966ebc792c5ed0d988eabec5f0b9
Lorivie Abapo <lorivieabapo@Lorivies-MacBook-Pro.local>
2022-10-12 15:57:33 -0600

    if/else statements

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 console.log ("hello world");
+//ACCESS GRANTED
+
+const allowedAge = 7;
+
+if(allowedAge >= 18 ) {
+    console.log("access granted. welcome aboard!")
+} else {
+    console.log("You are too young to access this content")
+};
+
+//Nested if/else statement
+let num= -3; 
+
+if(num > 100 ) {
+    console.log("More than 100")
+} else if(num < 100 ) {
+    console.log("Less than 100")
+} else (num>0) ;{
+    console.log ("Negative number")
+}
